bb08c5b93a883aa543e887bc2e262d2501bad215
Geomar Schreiner <geomarschreiner@gmail.com>
2018-09-14 09:05:08 -0300

    voltdb dependencies

---
+++ src/com/oltpbenchmark/benchmarks/ycsb/ddls/ycsb-voltdb-ddl.sql
@@ -0,0 +1,14 @@
+DROP TABLE USERTABLE IF EXISTS;
+CREATE TABLE USERTABLE (
+  YCSB_KEY INT PRIMARY KEY,
+  FIELD1 varchar(128), 
+  FIELD2 varchar(128),
+  FIELD3 varchar(128), 
+  FIELD4 varchar(128),
+  FIELD5 varchar(128), 
+  FIELD6 varchar(128),
+  FIELD7 varchar(128), 
+  FIELD8 varchar(128),
+  FIELD9 varchar(128), 
+  FIELD10 varchar(128)
+);
